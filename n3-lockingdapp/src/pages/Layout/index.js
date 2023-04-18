61daa16136621df0088569eb488e9461837e29ba
ryan08265 <ryanzerwas254@gmail.com>
2023-04-18 17:52:21 +0200

    UI: produce layout

--- n3-lockingdapp/src/pages/Layout/index.js
+++ n3-lockingdapp/src/pages/Layout/index.js
@@ -1,25 +1,29 @@
-import { Outlet, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Layout = () => {
+function Layout() {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/locking">Locking</Link>
-          </li>
-          <li>
-            <Link to="/deposit">Deposit</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
-    </>
-  )
-};
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/locking">Locking</Link></Nav.Link>
+            <Nav.Link><Link to="/deposit">Deposit</Link></Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
 export default Layout;
