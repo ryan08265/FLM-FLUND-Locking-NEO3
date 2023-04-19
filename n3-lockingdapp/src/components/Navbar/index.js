1801edce4b84723668ccade3f9933a696bc743ad
ryan08265 <ryanzerwas254@gmail.com>
2023-04-20 00:11:19 +0200

    feat: complete navbar

--- n3-lockingdapp/src/components/Navbar/index.js
+++ n3-lockingdapp/src/components/Navbar/index.js
@@ -1,23 +1,28 @@
 import { Link } from "react-router-dom";
-import { Container, Nav, Navbar as BsNavbar} from 'react-bootstrap';
-
+import { Container, Nav, NavItem, Navbar as BsNavbar} from 'react-bootstrap';
+import logo from '../../assets/images/logo.svg';
+import './index.scss';
 function Navbar() {
   return (
     <BsNavbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{ background: "#141414", borderBottom: "1px solid #707070"}}>
       <Container>
-        <BsNavbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Link
+					className='d-flex align-items-center navbar-brand mr-0 c-logo-container'
+					to="/"
+				>
+					<img src={logo} alt="Logo"/>
+				</Link>
+        <BsNavbar.Toggle aria-controls="responsive-navbar-nav" style={{ background: "#D8D3D3" }}/>
         <BsNavbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link><Link to="/">Home</Link></Nav.Link>
-            <Nav.Link><Link to="/locking">Locking</Link></Nav.Link>
-            <Nav.Link><Link to="/deposit">Deposit</Link></Nav.Link>
+            <Nav.Link className="custom-navbar-button custom-navbar-normal-button"><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link className="custom-navbar-button custom-navbar-normal-button"><Link to="/locking">Locking</Link></Nav.Link>
+            <Nav.Link className="custom-navbar-button custom-navbar-normal-button"><Link to="/deposit">Deposit</Link></Nav.Link>
           </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
+          <NavItem className='custom-navbar-button auth-button'>
+							Connect Wallet
+					</NavItem>
+          
         </BsNavbar.Collapse>
       </Container>
     </BsNavbar>
--- n3-lockingdapp/src/pages/Home/index.js
+++ n3-lockingdapp/src/pages/Home/index.js
@@ -3,21 +3,24 @@ import Card from 'react-bootstrap/Card';
 
 const Home = () => {
     return (
-      <Card
-          bg="primary"
-          text="white"
-          style={{ width: '18rem' }}
-          className="mb-2"
-        >
-          <Card.Header>Header</Card.Header>
-          <Card.Body>
-            <Card.Title> Locking Pool 1 </Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-      </Card>
+      <div>
+        <Card
+            bg="primary"
+            text="white"
+            style={{ width: '18rem' }}
+            className="mb-2"
+          >
+            <Card.Header>Locking Pool 1</Card.Header>
+            <Card.Body>
+              <Card.Title>  </Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+        </Card>
+      </div>
+      
     );
 }
   
