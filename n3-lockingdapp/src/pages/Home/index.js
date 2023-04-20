aedf84f73ff9a79538f930f84b5377e2b0c0eb8d
ryan08265 <ryanzerwas254@gmail.com>
2023-04-20 02:42:15 +0200

    feat: modify dashboard

--- n3-lockingdapp/src/pages/Home/index.js
+++ n3-lockingdapp/src/pages/Home/index.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-
+import { Card, Row, Col, Button } from 'react-bootstrap';
+import { lockingData } from "./data.js";
 const Home = () => {
     return (
-      <div>
-        <Card
-            bg="primary"
-            text="white"
-            style={{ width: '18rem' }}
-            className="mb-2"
-          >
-            <Card.Header>Locking Pool 1</Card.Header>
-            <Card.Body>
-              <Card.Title>  </Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-        </Card>
+      <div className = 'lockingwrapper'>
+        <Row xs={1} md={3} className="g-4">
+          {lockingData.map((data, idx) => (
+            <Col>
+              <Card
+                bg="primary"
+                text="white"
+                className="mb-2"
+              >
+              <Card.Header>Locking Pool {idx + 1}</Card.Header>
+                <Card.Body>
+                  <Card.Text>FLMAmount : {data.FLMAmount}</Card.Text>
+                  <Card.Text>FUSDTAward : {data.FUSDTAward}</Card.Text>
+                  <Card.Text>LockingStartTime : {data.lockingStatingTime}</Card.Text>
+                  <Card.Text>lockingFinishTime : {data.lockingFinishTime}</Card.Text>
+                  <Button bg="Success">Show more</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </div>
       
     );
