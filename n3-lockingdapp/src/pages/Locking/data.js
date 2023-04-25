61c944156a8a523a9638de99f66dc2407d7c6fe8
ryan08265 <ryanzerwas254@gmail.com>
2023-04-25 21:24:58 +0200

    feat: modify locking part

---
+++ n3-lockingdapp/src/pages/Locking/data.js
@@ -0,0 +1,32 @@
+export const lockingData = [
+    {
+        FLMAmount: "100.675",
+        FUSDTAward: "30",
+        lockingStatingTime: "2023.4.20:14:55:24",
+        lockingFinishTime: "2023.4.27:14:55:24"
+    },
+    {
+        FLMAmount: "300.542",
+        FUSDTAward: "70",
+        lockingStatingTime: "2023.4.20:17:54:24",
+        lockingFinishTime: "2023.4.24:17:54:24"
+    },
+    {
+        FLMAmount: "400.234",
+        FUSDTAward: "100",
+        lockingStatingTime: "2023.4.21:14:55:24",
+        lockingFinishTime: "2023.4.27:14:55:24"
+    },
+    {
+        FLMAmount: "220.500",
+        FUSDTAward: "21",
+        lockingStatingTime: "2023.4.22:14:55:24",
+        lockingFinishTime: "2023.4.24:14:55:24"
+    },
+    {
+        FLMAmount: "360.000",
+        FUSDTAward: "60",
+        lockingStatingTime: "2023.4.22:16:55:24",
+        lockingFinishTime: "2023.4.26:16:55:24"
+    },
+];
