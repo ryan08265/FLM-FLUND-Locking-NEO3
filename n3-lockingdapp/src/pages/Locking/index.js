06250220c823a5ed2a7b40a18f7fdae13dd427cc
ryan08265 <ryanzerwas254@gmail.com>
2023-04-25 19:06:44 +0200

    feat: add Locking part

--- n3-lockingdapp/src/pages/Locking/index.js
+++ n3-lockingdapp/src/pages/Locking/index.js
@@ -1,5 +1,32 @@
+import React from 'react';
+import { Card, Row, Col, Button } from 'react-bootstrap';
+import { lockingData } from "./data.js";
 const Locking = () => {
-    return <h1>Locking</h1>;
-  };
+    return (
+      <div className = 'lockingwrapper'>
+        <Row xs={1} md={3} className="g-4">
+          {lockingData.map((data, idx) => (
+            <Col>
+              <Card
+                bg="primary"
+                text="white"
+                className="mb-2"
+              >
+              <Card.Header>Locking Pool {idx + 1}</Card.Header>
+                <Card.Body>
+                  <Card.Text>FLMAmount : {data.FLMAmount}</Card.Text>
+                  <Card.Text>FUSDTAward : {data.FUSDTAward}</Card.Text>
+                  <Card.Text>LockingStartTime : {data.lockingStatingTime}</Card.Text>
+                  <Card.Text>LockingFinishTime : {data.lockingFinishTime}</Card.Text>
+                  <Button variant="danger">Show more</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
+      
+    );
+}
   
 export default Locking;
