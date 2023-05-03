f9865a33648d4a80894562ab1e444d73415af6dd
ryan08265 <ryanzerwas254@gmail.com>
2023-05-03 19:01:14 +0200

    feat: add FLM locking part for UI

--- n3-lockingdapp/src/pages/Locking/index.js
+++ n3-lockingdapp/src/pages/Locking/index.js
@@ -7,26 +7,6 @@ import { lockingData } from "./data.js";
 const Locking = () => {
     return (
       <div className = 'lockingwrapper'>
-        <Row xs={1} md={3} className="g-4">
-          {lockingData.map((data, idx) => (
-            <Col>
-              <Card
-                bg="primary"
-                text="white"
-                className="mb-2"
-              >
-              <Card.Header>Locking Pool {idx + 1}</Card.Header>
-                <Card.Body>
-                  <Card.Text>FLMAmount : {data.FLMAmount}</Card.Text>
-                  <Card.Text>FUSDTAward : {data.FUSDTAward}</Card.Text>
-                  <Card.Text>LockingStartTime : {data.lockingStatingTime}</Card.Text>
-                  <Card.Text>LockingFinishTime : {data.lockingFinishTime}</Card.Text>
-                  <Button variant="danger">Show more</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
         <div>
           <InputGroup className="mb-3">
             <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
