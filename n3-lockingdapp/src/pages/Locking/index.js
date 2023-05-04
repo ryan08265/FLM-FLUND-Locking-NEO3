7238628e7376e5bc17d20b06357337106ac4fb02
ryan08265 <ryanzerwas254@gmail.com>
2023-05-04 18:26:20 +0200

    feat: modify locking part

--- n3-lockingdapp/src/pages/Locking/index.js
+++ n3-lockingdapp/src/pages/Locking/index.js
@@ -32,43 +32,15 @@ const Locking = () => {
               onChange={(e) => setFUSDTAward(e.target.value)}
             />
           </InputGroup>
-          <InputGroup className="mb-3">
-            <InputGroup.Text id="basic-addon1">Locking Period</InputGroup.Text>
+          <InputGroup>
+            <InputGroup.Text id="basic-addon1">Set Locking Period</InputGroup.Text>
             <Form.Control
-              placeholder="Username"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
+              placeholder="Recipient's username"
+              aria-label="Recipient's username with two button addons"
               value={lockingPeriod}
               onChange={(e) => setLockingPeriod(e.target.value)}
             />
-          </InputGroup>
-
-          <InputGroup className="mb-3">
-            <Form.Control
-              placeholder="Recipient's username"
-              aria-label="Recipient's username"
-              aria-describedby="basic-addon2"
-            />
-            <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
-          </InputGroup>
-
-          <Form.Label htmlFor="basic-url">Your vanity URL</Form.Label>
-          <InputGroup className="mb-3">
-            <InputGroup.Text id="basic-addon3">
-              https://example.com/users/
-            </InputGroup.Text>
-            <Form.Control id="basic-url" aria-describedby="basic-addon3" />
-          </InputGroup>
-
-          <InputGroup className="mb-3">
-            <InputGroup.Text>$</InputGroup.Text>
-            <Form.Control aria-label="Amount (to the nearest dollar)" />
-            <InputGroup.Text>.00</InputGroup.Text>
-          </InputGroup>
-
-          <InputGroup>
-            <InputGroup.Text>With textarea</InputGroup.Text>
-            <Form.Control as="textarea" aria-label="With textarea" />
+            <Button variant="outline-secondary">Begin New Locking</Button>
           </InputGroup>
         </div>
       </div>
