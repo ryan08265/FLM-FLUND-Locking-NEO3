6741f3a7601a165bf091f61f96bf85ca1b28efc2
ryan08265 <ryanzerwas254@gmail.com>
2023-05-05 18:47:00 +0200

    modify dashboard

--- n3-lockingdapp/src/pages/Home/index.js
+++ n3-lockingdapp/src/pages/Home/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Card, Row, Col, Button } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
 import { lockingData } from "./data.js";
 
 
 const Home = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShowMore = () => setShow(true);
     return (
       <div className = 'lockingwrapper'>
         <Row xs={1} md={3} className="g-4">
@@ -20,12 +24,26 @@ const Home = () => {
                   <Card.Text>FUSDTAward : {data.FUSDTAward}</Card.Text>
                   <Card.Text>LockingStartTime : {data.lockingStatingTime}</Card.Text>
                   <Card.Text>LockingFinishTime : {data.lockingFinishTime}</Card.Text>
-                  <Button variant="danger">Show more</Button>
+                  <Button variant="danger" onClick={handleShowMore}>Show more</Button>
                 </Card.Body>
               </Card>
             </Col>
           ))}
         </Row>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
       
     );
