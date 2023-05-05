7a9cfbb70a607124079db3324022a0ebc501846c
ryan08265 <ryanzerwas254@gmail.com>
2023-05-05 21:43:00 +0200

    feat: modify deposit UI

--- n3-lockingdapp/src/pages/Deposit/data.js
+++ n3-lockingdapp/src/pages/Deposit/data.js
@@ -3,25 +3,30 @@ export const lockingData = [
         FLMAmount: "100.675",
         FUSDTAward: "30",
         lockingPeriod: "5 days",
+        lockingUser: "NftS1gfUFrJ46oByhpVavEBB1ywmNUKZ5h"
     },
     {
         FLMAmount: "300.542",
         FUSDTAward: "70",
-        lockingPeriod: "7 days"
+        lockingPeriod: "7 days",
+        lockingUser: "NftS1gfUFrJ46oByhpVavEBB1ywmNUKZ5h"
     },
     {
         FLMAmount: "400.234",
         FUSDTAward: "100",
-        lockingPeriod: "3 days"
+        lockingPeriod: "3 days",
+        lockingUser: "NftS1gfUFrJ46oByhpVavEBB1ywmNUKZ5h"
     },
     {
         FLMAmount: "220.500",
         FUSDTAward: "21",
-        lockingPeriod: "20 hours"
+        lockingPeriod: "20 hours",
+        lockingUser: "NftS1gfUFrJ46oByhpVavEBB1ywmNUKZ5h"
     },
     {
         FLMAmount: "360.000",
         FUSDTAward: "60",
-        lockingPeriod: "10 days"
+        lockingPeriod: "10 days",
+        lockingUser: "NftS1gfUFrJ46oByhpVavEBB1ywmNUKZ5h"
     },
 ];
--- n3-lockingdapp/src/pages/Deposit/index.js
+++ n3-lockingdapp/src/pages/Deposit/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Card, Row, Col, Button } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
+
 import { lockingData } from "./data.js";
 const Deposit = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const [info, setInfo] = useState([]);
+  const handleShowMore = (data) => {
+    setInfo(data)
+    setShow(true);
+  }
+  const handleDeposit = (e) => {
+    e.preventDefault();
+  }
   return (
       <div className = 'lockingwrapper'>
         <Row xs={1} md={3} className="g-4">
@@ -17,13 +29,36 @@ const Deposit = () => {
                   <Card.Text>FLMAmount : {data.FLMAmount}</Card.Text>
                   <Card.Text>FUSDTAward : {data.FUSDTAward}</Card.Text>
                   <Card.Text>LockingPeriod : {data.lockingPeriod}</Card.Text>
-                  <Button variant="danger">Lock funds!</Button>
+                  <Button variant="danger" onClick={() => handleShowMore(data)}>Lock funds!</Button>
                 </Card.Body>
               </Card>
             </Col>
           ))}
         </Row>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Locking Pool Status</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Card.Body>
+              <Card.Text>FLMAmount : {info.FLMAmount}</Card.Text>
+              <Card.Text>FUSDTAward : {info.FUSDTAward}</Card.Text>
+              <Card.Text>LockingPeriod : {info.lockingPeriod}</Card.Text>
+              <Card.Text>LockingUser : {info.lockingUser}</Card.Text>
+              <Card.Text>FLM:FLUND : </Card.Text>
+            </Card.Body>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleDeposit}>
+              Deposit Now!
+            </Button>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
+      
       
     );
   };
